9627b289d65bc2a3b65a861befe82e3c942b5ff5
Wanderson <wandersonwhcr@gmail.com>
2020-05-11 22:33:52 -0300

    Corrige inicialização do serviço

--- server.js
+++ server.js
@@ -1,3 +1,9 @@
 const server = require('.');
 
-server.listen(process.env.PORT, '0.0.0.0');
+server.listen(process.env.PORT, '0.0.0.0')
+  .catch((error) => {
+    // Problema Encontrado!
+    console.error(error);
+    // Finalização
+    process.exit(255);
+  });

0b74ca8be8141c0562e3cc12a85a05c5fbdc30a2
Wanderson <wandersonwhcr@gmail.com>
2020-05-11 23:56:04 -0300

    Efetua a conexão com o banco de dados

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 const fastify = require('fastify')({ logger: true });
 
-fastify.get('/', async (req, rep) => {
-  return {
-    name: '@wandersonwhcr/playground',
-    version: '1.0.0-alpha',
-  };
+fastify.register(require('fastify-mongodb'), {
+  forceClose: true,
+  url: process.env.MONGO_URL,
 });
 
 fastify.register(require('./routes/v1/games'), { prefix: 'v1' });

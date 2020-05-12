067a055f7a350d5a169f78b98382ab2e112063a5
Wanderson <wandersonwhcr@gmail.com>
2020-05-11 22:42:54 -0300

    Inicializa rota principal

--- index.js
+++ index.js
@@ -2,4 +2,11 @@ const fastify = require('fastify');
 
 const server = fastify({ logger: true });
 
+server.get('/', async (req, rep) => {
+  return {
+    name: '@wandersonwhcr/playground',
+    version: '1.0.0-alpha',
+  };
+});
+
 module.exports = server;

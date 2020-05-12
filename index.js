1bd37e96046ef334b38f0d7e04bf6f94afa49111
Wanderson <wandersonwhcr@gmail.com>
2020-05-12 12:12:46 -0300

    Adiciona tratamento de erros

--- index.js
+++ index.js
@@ -10,4 +10,16 @@ fastify.addSchema(require('./schemas/v1/games'));
 
 fastify.register(require('./routes/v1/games'));
 
+fastify.setErrorHandler(async function ErrorHandler(error, request, reply) {
+  // Código?
+  switch (error.code) {
+    case 11000:
+      // Conflict
+      reply.code(409);
+      break;
+  }
+  // Apresentação
+  reply.send();
+});
+
 module.exports = fastify;

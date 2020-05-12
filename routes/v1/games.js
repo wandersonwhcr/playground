a55bc653ce0ea43ceed3a69d1e453d89e017ac70
Wanderson <wandersonwhcr@gmail.com>
2020-05-11 23:30:59 -0300

    Possibilita inserção de jogos

--- routes/v1/games.js
+++ routes/v1/games.js
@@ -21,12 +21,36 @@ module.exports = function routes(fastify, options, done) {
         },
       },
     },
-    handler: async function ToysFetch(request, reply) {
+    handler: async function GamesFetch(request, reply) {
       return [
         {
           "name": "Chrono Trigger",
         },
       ];
+    },
+  });
+
+  // Adicionar
+  fastify.route({
+    method: 'PUT',
+    url: '/games/:gameId',
+    schema: {
+      response: {
+        200: {
+          type: 'object',
+          required: ['_id'],
+          additionalProperties: false,
+          properties: {
+            _id: {
+              type: 'string',
+              maxLength: 1,
+            },
+          },
+        },
+      },
+    },
+    handler: async function GamesInsert(request, reply) {
+      return { _id: request.params.gameId };
     },
   });
 

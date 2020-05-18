e439d205d61a4ed4bcd92ffb429f5ebf7add432f
Wanderson <wandersonwhcr@gmail.com>
2020-05-18 18:59:14 -0300

    Cria rota para listar publicadoras

--- routes/v1/publishers.js
+++ routes/v1/publishers.js
@@ -1,4 +1,15 @@
 module.exports = function publishers(fastify, options, done) {
+  // Pesquisar
+  fastify.route({
+    method: 'GET',
+    url: '/v1/publishers',
+    handler: async function PublishersFetch(request, reply) {
+      return this.mongo.games.db
+        .collection('publishers')
+        .find()
+        .toArray();
+    },
+  });
   // Feito!
   done();
 };

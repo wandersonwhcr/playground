82f187c7ecaf3252de81b500afa82deabca07930
Wanderson <wandersonwhcr@gmail.com>
2020-05-18 19:05:25 -0300

    Insere uma nova publicadora

--- routes/v1/publishers.js
+++ routes/v1/publishers.js
@@ -1,3 +1,5 @@
+const uuid = require('uuid');
+
 module.exports = function publishers(fastify, options, done) {
   // Pesquisar
   fastify.route({
@@ -10,6 +12,32 @@ module.exports = function publishers(fastify, options, done) {
         .toArray();
     },
   });
+
+  // Adicionar
+  fastify.route({
+    method: 'POST',
+    url: '/v1/publishers',
+    schema: {
+      body: {
+        $ref: '/v1/publishers',
+      },
+    },
+    handler: async function PublishersInsert(request, reply) {
+      // Identificador
+      const identifier = { _id: uuid.v4() };
+      // Configuração
+      const data = Object.assign({}, identifier, request.body);
+      // Inserir
+      const result = await this.mongo.games.db
+        .collection('publishers')
+        .insertOne(data);
+      // Configuração
+      reply.code(201);
+      // Apresentação
+      return identifier;
+    },
+  });
+
   // Feito!
   done();
 };
